9c5fdd6a9a2df4c2f5e7dd37e678c5a684333319
Ovidiu Luca <ov.luca@gmail.com>
2018-07-28 22:44:53 +0300

    In App Billing

--- app/src/main/java/com/example/shop/activities/ShoppingCartActivity.kt
+++ app/src/main/java/com/example/shop/activities/ShoppingCartActivity.kt
@@ -4,10 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.BillingClientStateListener
-import com.android.billingclient.api.Purchase
-import com.android.billingclient.api.PurchasesUpdatedListener
+import android.util.Log
+import android.view.View
+import com.android.billingclient.api.*
 import com.example.shop.R
 import com.example.shop.adapters.ShoppingCartAdapter
 import com.example.shop.contracts.ShoppingCartContract
@@ -17,9 +16,10 @@ import kotlinx.android.synthetic.main.activity_base.*
 import kotlinx.android.synthetic.main.activity_shopping_cart.*
 import kotlinx.android.synthetic.main.app_bar_home.*
 
-class ShoppingCartActivity : BaseActivity(), ShoppingCartContract.IShoppingCartView, PurchasesUpdatedListener {
+
+class ShoppingCartActivity : BaseActivity(), ShoppingCartContract.IShoppingCartView, View.OnClickListener, PurchasesUpdatedListener {
     override fun onPurchasesUpdated(responseCode: Int, purchases: MutableList<Purchase>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d("Billing", "onPurchasesUpdated")
     }
 
 
@@ -44,7 +44,8 @@ class ShoppingCartActivity : BaseActivity(), ShoppingCartContract.IShoppingCartV
         shopping_cart_recycler.layoutManager = LinearLayoutManager(this)
         shopping_cart_recycler.adapter = ShoppingCartAdapter(this, products)
 
-        initBilling()
+        shopping_cart_check_out_btn.setOnClickListener(this)
+
     }
 
     override fun onResume() {
@@ -59,6 +60,16 @@ class ShoppingCartActivity : BaseActivity(), ShoppingCartContract.IShoppingCartV
         this.shopping_cart_recycler.adapter.notifyDataSetChanged()
     }
 
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            shopping_cart_check_out_btn.id -> {
+                initBilling()
+                return
+            }
+
+        }
+    }
+
     companion object {
         fun open(context: Context) {
             val intent = Intent(context, ShoppingCartActivity::class.java)
@@ -66,18 +77,50 @@ class ShoppingCartActivity : BaseActivity(), ShoppingCartContract.IShoppingCartV
         }
     }
 
-    fun initBilling(){
+    private fun initBilling() {
         billingClient = BillingClient.newBuilder(this).setListener(this).build()
+
         billingClient.startConnection(object : BillingClientStateListener {
             override fun onBillingSetupFinished(@BillingClient.BillingResponse billingResponseCode: Int) {
-                if (billingResponseCode == BillingClient.BillingResponse.OK) {
-                    // The billing client is ready. You can query purchases here.
+                when (billingResponseCode) {
+                    BillingClient.BillingResponse.OK -> {
+                        Log.d("Billing", "BillingResponse.OK")
+                        queryInAppProducts()
+                    }
                 }
             }
+
             override fun onBillingServiceDisconnected() {
                 // Try to restart the connection on the next request to
                 // Google Play by calling the startConnection() method.
             }
         })
+    }
+
+    private fun queryInAppProducts() {
+
+        val skuList = ArrayList<String>()
+        skuList.add("product")
+
+        val params = SkuDetailsParams.newBuilder()
+        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+
+        billingClient.querySkuDetailsAsync(params.build(),
+                object : SkuDetailsResponseListener {
+                    override fun onSkuDetailsResponse(responseCode: Int, skuDetailsList: MutableList<SkuDetails>?) {
+
+                        Log.d("Billing", "onSkuDetailsResponse")
+                        val flowParams = BillingFlowParams.newBuilder()
+                                .setSku("product")
+                                .setType(BillingClient.SkuType.INAPP) // SkuType.SUB for subscription
+                                .build()
+                        val responseCode = billingClient.launchBillingFlow(
+                                this@ShoppingCartActivity,
+                                flowParams
+                        )
+
+                    }
+
+                })
     }
 }
